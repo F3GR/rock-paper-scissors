fd4a9447d958a01d5a4abdeb79e011220c69f9a5
F3GR <egmver.2@gmail.com>
2023-06-21 21:57:10 +0400

    added message about current scores

--- game.js
+++ game.js
@@ -1,11 +1,20 @@
 const choiceRock = "rock";
 const choicePaper = "paper";
 const choiceScissors = "scissors";
+let rounds = 0;
+let winsPlayer = 0;
+let winsComputer = 0;
 
 let playerChoice = getUserChoice();
 let computerChoice = getComputerChoice();
-let rounds = 0;
 
+console.log(playRound(playerChoice, computerChoice));
+
+printWins();
+
+function printWins() {
+    console.log(`Player's score: ${winsPlayer}, Computer's score: ${winsComputer}.`)
+}
 
 function getUserChoice() {
     const userChoice = prompt("Choose rock, paper or scissors: ").toLowerCase();
@@ -27,27 +36,33 @@ function getComputerChoice() {
     } 
 }
 
-function gameRound(playerChoice, computerChoice) {
+function playRound(playerChoice, computerChoice) {
     switch (true) {
         case (playerChoice === computerChoice):
             return `Tie! Your choice is ${playerChoice}, Computer's choice is ${computerChoice} as well.`;
         case (playerChoice === "rock", computerChoice === "paper"):
             rounds++;
+            winsComputer++;
             return "You Lose! Rock loses to Scissors";
         case (playerChoice === "rock", computerChoice === "scissors"):
             rounds++;
+            winsPlayer++;
             return "You Win! Rock beats Scissors";
         case (playerChoice === "paper", computerChoice === "rock"):
             rounds++;
+            winsPlayer++;
             return "You Win! Paper beats Rock";
         case (playerChoice === "paper", computerChoice === "scissors"):
             rounds++;
+            winsComputer++;
             return "You Lose! Paper loses to Scissors";
         case (playerChoice === "scissors", computerChoice === "rock"):
             rounds++;
+            winsComputer++;
             return "You Lose! Scissors lose to Rock";
         case (playerChoice === "scissors", computerChoice === "paper"):
             rounds++;
+            winsPlayer++;
             return "You Win! Scissors beat Paper";
     }
 }
