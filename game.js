5c8d8f585940d5ef27e6a64ea04f7865e0a4ace2
F3GR <egmver.2@gmail.com>
2023-06-21 21:28:16 +0400

    added user choice

---
+++ game.js
@@ -0,0 +1,33 @@
+const choiceRock = "rock";
+const choicePaper = "paper";
+const choiceScissors = "scissors";
+
+let playerChoice = getUserChoice();
+
+let computerChoice = getComputerChoice();
+
+
+
+function getUserChoice() {
+    let userChoice;
+    while (userChoice !== choiceRock && userChoice !== choicePaper && userChoice !== choiceScissors) {
+        userChoice = prompt("Choose rock, paper or scissors: ");
+        userChoice = userChoice.toLowerCase();
+        if (userChoice !== choiceRock && userChoice !== choicePaper && userChoice !== choiceScissors) {
+            alert("Your input is not valid, please try again.");
+            getUserChoice();
+        }
+    }
+    return userChoice;
+}
+
+function getComputerChoice() {
+    let computerChoice = Math.random() * 3 + 1;
+    if (computerChoice == 1) {
+        return choiceRock;
+    } else if (computerChoice == 2) {
+        return choicePaper;
+    } else {
+        return choiceScissors;
+    } 
+}
