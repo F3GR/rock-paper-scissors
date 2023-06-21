35b0000a88c25a68cabe38a99c293cec00a2bb03
F3GR <egmver.2@gmail.com>
2023-06-21 21:51:38 +0400

    added a round:

--- game.js
+++ game.js
@@ -4,11 +4,11 @@ const choiceScissors = "scissors";
 
 let playerChoice = getUserChoice();
 let computerChoice = getComputerChoice();
+let rounds = 0;
 
 
 function getUserChoice() {
-    let userChoice;
-    userChoice = prompt("Choose rock, paper or scissors: ").toLowerCase();
+    const userChoice = prompt("Choose rock, paper or scissors: ").toLowerCase();
     if (userChoice !== choiceRock && userChoice !== choicePaper && userChoice !== choiceScissors) {
         alert("Your input is not valid, please try again.");
         getUserChoice();
@@ -17,7 +17,7 @@ function getUserChoice() {
 }
 
 function getComputerChoice() {
-    let computerChoice = Math.round(Math.random() * 3) + 1;
+    const computerChoice = Math.round(Math.random() * 3) + 1;
     if (computerChoice === 1) {
         return choiceRock;
     } else if (computerChoice === 2) {
@@ -29,6 +29,25 @@ function getComputerChoice() {
 
 function gameRound(playerChoice, computerChoice) {
     switch (true) {
-        case ()
+        case (playerChoice === computerChoice):
+            return `Tie! Your choice is ${playerChoice}, Computer's choice is ${computerChoice} as well.`;
+        case (playerChoice === "rock", computerChoice === "paper"):
+            rounds++;
+            return "You Lose! Rock loses to Scissors";
+        case (playerChoice === "rock", computerChoice === "scissors"):
+            rounds++;
+            return "You Win! Rock beats Scissors";
+        case (playerChoice === "paper", computerChoice === "rock"):
+            rounds++;
+            return "You Win! Paper beats Rock";
+        case (playerChoice === "paper", computerChoice === "scissors"):
+            rounds++;
+            return "You Lose! Paper loses to Scissors";
+        case (playerChoice === "scissors", computerChoice === "rock"):
+            rounds++;
+            return "You Lose! Scissors lose to Rock";
+        case (playerChoice === "scissors", computerChoice === "paper"):
+            rounds++;
+            return "You Win! Scissors beat Paper";
     }
 }
