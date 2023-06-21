9272099502ec33c06412165eff4884e80267e850
F3GR <egmver.2@gmail.com>
2023-06-21 22:02:24 +0400

    added round's loop

--- game.js
+++ game.js
@@ -1,19 +1,15 @@
 const choiceRock = "rock";
 const choicePaper = "paper";
 const choiceScissors = "scissors";
-let rounds = 0;
+const winScore = 5;
 let winsPlayer = 0;
 let winsComputer = 0;
 
-let playerChoice = getUserChoice();
-let computerChoice = getComputerChoice();
-
-console.log(playRound(playerChoice, computerChoice));
-
-printWins();
-
-function printWins() {
-    console.log(`Player's score: ${winsPlayer}, Computer's score: ${winsComputer}.`)
+while (winsPlayer !== winScore && winsComputer !== winScore) {
+    let playerChoice = getUserChoice();
+    let computerChoice = getComputerChoice();
+    alert(playRound(playerChoice, computerChoice));
+    printWins();
 }
 
 function getUserChoice() {
@@ -41,28 +37,26 @@ function playRound(playerChoice, computerChoice) {
         case (playerChoice === computerChoice):
             return `Tie! Your choice is ${playerChoice}, Computer's choice is ${computerChoice} as well.`;
         case (playerChoice === "rock", computerChoice === "paper"):
-            rounds++;
             winsComputer++;
             return "You Lose! Rock loses to Scissors";
         case (playerChoice === "rock", computerChoice === "scissors"):
-            rounds++;
             winsPlayer++;
             return "You Win! Rock beats Scissors";
         case (playerChoice === "paper", computerChoice === "rock"):
-            rounds++;
             winsPlayer++;
             return "You Win! Paper beats Rock";
         case (playerChoice === "paper", computerChoice === "scissors"):
-            rounds++;
             winsComputer++;
             return "You Lose! Paper loses to Scissors";
         case (playerChoice === "scissors", computerChoice === "rock"):
-            rounds++;
             winsComputer++;
             return "You Lose! Scissors lose to Rock";
         case (playerChoice === "scissors", computerChoice === "paper"):
-            rounds++;
             winsPlayer++;
             return "You Win! Scissors beat Paper";
     }
+}
+
+function printWins() {
+    console.log(`Your score: ${winsPlayer}, Computer's score: ${winsComputer}.`)
 }
