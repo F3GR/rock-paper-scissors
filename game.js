1208e949a7817914419b6dfacb3acd077f8a141a
F3GR <egmver.2@gmail.com>
2023-06-21 21:40:15 +0400

    edit userChoice

--- game.js
+++ game.js
@@ -3,29 +3,23 @@ const choicePaper = "paper";
 const choiceScissors = "scissors";
 
 let playerChoice = getUserChoice();
-
 let computerChoice = getComputerChoice();
-
-
 
 function getUserChoice() {
     let userChoice;
-    while (userChoice !== choiceRock && userChoice !== choicePaper && userChoice !== choiceScissors) {
-        userChoice = prompt("Choose rock, paper or scissors: ");
-        userChoice = userChoice.toLowerCase();
-        if (userChoice !== choiceRock && userChoice !== choicePaper && userChoice !== choiceScissors) {
-            alert("Your input is not valid, please try again.");
-            getUserChoice();
-        }
+    userChoice = prompt("Choose rock, paper or scissors: ").toLowerCase();
+    if (userChoice !== choiceRock && userChoice !== choicePaper && userChoice !== choiceScissors) {
+        alert("Your input is not valid, please try again.");
+        getUserChoice();
     }
     return userChoice;
 }
 
 function getComputerChoice() {
-    let computerChoice = Math.random() * 3 + 1;
-    if (computerChoice == 1) {
+    let computerChoice = Math.round(Math.random() * 3) + 1;
+    if (computerChoice === 1) {
         return choiceRock;
-    } else if (computerChoice == 2) {
+    } else if (computerChoice === 2) {
         return choicePaper;
     } else {
         return choiceScissors;
