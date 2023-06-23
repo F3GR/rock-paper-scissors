9254150465751a54db394b24c5bd770a0281763f
F3GR <egmver.2@gmail.com>
2023-06-23 23:57:33 +0400

    added eventListeners

--- input.js
+++ input.js
@@ -49,11 +49,13 @@ function promptAgainIfNeeded(inputField, message) {
 
 
 
-/***** Game script *****/
+/***** Game Interface *****/
 
 function playGame() {
 
+    const choiceOptions = ["rock", "paper", "scissors"];
     const winScore = 5;
+
     let winsPlayer = 0;
     let winsComputer = 0;
     let currentRound = 1;
@@ -116,7 +118,7 @@ function playGame() {
     computerScore.innerText = `${winsComputer}`;
     scoreComputer.appendChild(computerScore);
 
-
+    // Previous round's result
     const roundResult = document.createElement("div");
     roundResult.setAttribute("class", "round-result");
     mainHTML.appendChild(roundResult);
@@ -126,7 +128,7 @@ function playGame() {
     choices.setAttribute("class", "choices");
     mainHTML.appendChild(choices);
 
-
+    // Button Rock
     const choiceRock = document.createElement("button");
     choiceRock.setAttribute("class", "rock");
     choices.appendChild(choiceRock);
@@ -136,7 +138,8 @@ function playGame() {
     imageRock.setAttribute("alt", "An image of choice 'Rock'");
     imageRock.setAttribute("title", "Click on the image to make your choice!");
     choiceRock.appendChild(imageRock);
-
+    
+    // Button Paper
     const choicePaper = document.createElement("button");
     choicePaper.setAttribute("class", "paper");
     choices.appendChild(choicePaper);
@@ -147,6 +150,7 @@ function playGame() {
     imagePaper.setAttribute("title", "Click on the image to make your choice!");
     choicePaper.appendChild(imagePaper);
 
+    // Button Scissors
     const choiceScissors = document.createElement("button");
     choiceScissors.setAttribute("class", "scissors");
     choices.appendChild(choiceScissors);
@@ -157,4 +161,79 @@ function playGame() {
     imageScissors.setAttribute("title", "Click on the image to make your choice!");
     choiceScissors.appendChild(imageScissors);
 
+
+    while (winsPlayer !== winScore && winsComputer !== winScore) {
+        let playerChoice = getUserChoice();
+        let computerChoice = getComputerChoice();
+        alert(playRound(playerChoice, computerChoice));
+        printScore();
+        }
+        printWinner();
+
+        choiceRock.addEventListener("keydown", getUserChoice(e));
+        choiceRock.addEventListener("click", getUserChoice(e));
+        choicePaper.addEventListener("keydown", getUserChoice(e));
+        choicePaper.addEventListener("click", getUserChoice(e));
+        choiceScissors.addEventListener("keydown", getUserChoice(e));
+        choiceScissors.addEventListener("click", getUserChoice(e));
+        
+        function getUserChoice(e) {
+            if (e === "click") {
+                return objectName.className;
+            }
+            if (e === "keydown") {
+                if (e.key === 'Enter') {
+                    return objectName.className;
+                }
+            }
+        }
+        
+        function getComputerChoice() {
+        const computerChoice = Math.round(Math.random() * 3) + 1;
+        if (computerChoice === 1) {
+            return choiceOptions[0];
+        } else if (computerChoice === 2) {
+            return choiceOptions[1];
+        } else {
+            return choiceOptions[2];
+        } 
+        }
+        
+        function playRound(playerChoice, computerChoice) {
+        switch (true) {
+            case (playerChoice === computerChoice):
+            return `Tie! Your choice is ${playerChoice}, Computer's choice is ${computerChoice} as well.`;
+            case (playerChoice === choiceOptions[0], computerChoice === choiceOptions[1]):
+            winsComputer++;
+            return "You Lose! Rock loses to Scissors";
+            case (playerChoice === choiceOptions[0], computerChoice === choiceOptions[2]):
+            winsPlayer++;
+            return "You Win! Rock beats Scissors";
+            case (playerChoice === choiceOptions[1], computerChoice === choiceOptions[0]):
+            winsPlayer++;
+            return "You Win! Paper beats Rock";
+            case (playerChoice === choiceOptions[1], computerChoice === choiceOptions[2]):
+            winsComputer++;
+            return "You Lose! Paper loses to Scissors";
+            case (playerChoice === choiceOptions[2], computerChoice === choiceOptions[0]):
+            winsComputer++;
+            return "You Lose! Scissors lose to Rock";
+            case (playerChoice === choiceOptions[2], computerChoice === choiceOptions[1]):
+            winsPlayer++;
+            return "You Win! Scissors beat Paper";
+        }
+        }
+        
+        function printScore() {
+        console.log(`Your current score: ${winsPlayer}, Computer's current score: ${winsComputer}.`);
+        }
+        
+        function printWinner() {
+        if (winsPlayer === winScore) {
+            alert("Congratulations! You won the game!");
+        }
+        if (winsComputer === winScore) {
+            alert("Game over! Computer won the game.");
+        }
+    }
 }
