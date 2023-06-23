3110aff7e0c38e7e48f0fa52b5fe528134dc998d
F3GR <egmver.2@gmail.com>
2023-06-23 18:30:10 +0400

    created input menu and greeting page

---
+++ input.js
@@ -0,0 +1,38 @@
+/*********
+
+Input script 
+
+*********/
+
+const nameInput = document.querySelector('.form input');
+const invalidInputMessage = document.querySelector('.incorrect-input-message');
+let playerName;
+
+nameInput.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+            playerName = promptAgainIfNeeded(nameInput, invalidInputMessage);
+    }
+});
+
+function notValid(value) {
+    return (value === null || value === undefined || value.trim() === '');
+}
+
+function promptAgainIfNeeded(inputField, message) {
+    if (notValid(inputField.value)) {
+        invalidInputMessage.innerText = "Your input is not valid. Please, try again!";
+    } else {
+        let name = inputField.value;
+        inputField.value = "";
+        inputField.innerText = "";
+        message.innerText = "";
+        return name;
+    } 
+}
+
+
+/*********
+
+Game script 
+
+*********/
